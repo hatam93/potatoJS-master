57b1638eef99122ed06693f7a629782a4305389c
hatam93 <71016328+hatam93@users.noreply.github.com>
2020-10-27 17:12:14 +0600

    Add files via upload

---
+++ potatoJS-master/js/base.js
@@ -0,0 +1,5 @@
+export const 
+container = document.createElement('div');
+container.className = 'container';
+container.style.maxWidth = '1170px';
+container.style.margin = '0 auto';
---
+++ potatoJS-master/js/components/preloader/index.js
@@ -0,0 +1,7 @@
+const overlay = document.createElement('div');
+
+overlay.className = 'preloader';
+
+overlay.innerHTML = `<h1>OCOLUS<span>.</span></h1>`;
+
+export default overlay;
